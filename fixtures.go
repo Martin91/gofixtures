c56c47ebe98cdc6c86b9e4befe366eefb1207a24
Martin Hong <hongzeqin@gmail.com>
2021-04-18 12:47:40 +0800

    initialize the project

---
+++ fixtures.go
@@ -0,0 +1,37 @@
+// Package fixtures implements a Ruby on Rails style test fixtures suite
+package fixtures
+
+import "database/sql"
+
+// Fixtures an utility to hold definitions of fixtures
+type Fixtures struct {
+	path     string             // path to load yaml definition files recursively
+	db       *sql.Conn          // db holds an database connection
+	fixtures map[string]Fixture // fixtures loaded and parsed definitions
+}
+
+func (f *Fixtures) Load() error {
+	return nil
+}
+
+type CallbackType uint8
+
+const (
+	BeforeCreate CallbackType = iota
+	AfterCreate
+)
+
+type CallbackFunc func(*Fixture) error
+
+type Fixture struct {
+	Columns   map[string]interface{}          // Columns fields and values of an single object
+	Callbacks map[CallbackType][]CallbackFunc // Callbacks
+}
+
+func Load(path string, db *sql.Conn) (*Fixtures, error) {
+	fixtures := &Fixtures{
+		path: path,
+		db:   db,
+	}
+	return fixtures, fixtures.Load()
+}
---
+++ fixtures_test.go
@@ -0,0 +1,47 @@
+package fixtures
+
+import (
+	"database/sql"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestLoad(t *testing.T) {
+	type args struct {
+		path string
+		db   *sql.Conn
+	}
+	tests := []struct {
+		name        string
+		args        args
+		wantErr     bool
+		judgeResult bool
+	}{
+		{
+			name: "unexisted path",
+			args: args{
+				path: "path/not/existed",
+			},
+			wantErr: true,
+		},
+		{
+			name: "existed directory path",
+			args: args{
+				path: "dummy",
+			},
+			judgeResult: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fixtures, err := Load(tt.args.path, tt.args.db)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Load() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if tt.judgeResult {
+				assert.NotEmpty(t, fixtures.fixtures["coupons.twenty_discount"])
+			}
+		})
+	}
+}
